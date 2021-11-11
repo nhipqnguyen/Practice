d5c001838e6e499b59d645b04a29f8918e069eaa
nhipqnguyen <quynhi2106@gmail.com>
2021-11-10 22:44:04 -0800

    coding interview exercises

---
+++ coding_interview_prep.js
@@ -0,0 +1,42 @@
+// Find the symmetric difference
+function sym(args) {
+  return args;
+}
+
+sym([1, 2, 3], [5, 2, 1, 4]);
+
+// Inventory Update
+function updateInventory(arr1, arr2) {
+  return arr1;
+}
+
+// Example inventory lists
+var curInv = [
+  [21, "Bowling Ball"],
+  [2, "Dirty Sock"],
+  [1, "Hair Pin"],
+  [5, "Microphone"]
+];
+
+var newInv = [
+  [2, "Hair Pin"],
+  [3, "Half-Eaten Apple"],
+  [67, "Bowling Ball"],
+  [7, "Toothpaste"]
+];
+
+updateInventory(curInv, newInv);
+
+// No repeat please
+function permAlone(str) {
+  return str;
+}
+
+permAlone('aab');
+
+// Pairwise
+function pairwise(arr, arg) {
+  return arg;
+}
+
+pairwise([1,4,2,3,0,5], 7);
