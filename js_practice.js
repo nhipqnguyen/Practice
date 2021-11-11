4a3b0e94308c9c72a49c74f78a1ff3279ef7e7c5
nhipqnguyen <quynhi2106@gmail.com>
2021-11-10 22:40:41 -0800

    practice basic js

---
+++ js_practice.js
@@ -0,0 +1,47 @@
+var myName = "Nhi";
+// Variable declarations
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+// Variable assignments
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+tITLEcASEoVER = 9000;
+
+// Setup
+var a;
+a = 7;
+var b;
+
+var fCC = "freeCodeCamp"; // Change this line
+var fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(fCC, fact); // Change this line
+
+let myVar = 87;
+
+// Only change code below this line
+myVar = myVar + 1;
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+// Setup
+function testStrict(val) {
+    if (val) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
