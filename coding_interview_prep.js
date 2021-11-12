a63deed3749c55bbd72c45996aae4ca6080aa610
nhipqnguyen <quynhi2106@gmail.com>
2021-11-11 22:04:26 -0800

    excerises on data structure

--- coding_interview_prep.js
+++ coding_interview_prep.js
@@ -40,3 +40,90 @@ function pairwise(arr, arg) {
 }
 
 pairwise([1,4,2,3,0,5], 7);
+
+// Perform an Intersection on Two Sets of Data
+class Set {
+  constructor() {
+    // This will hold the set
+    this.dictionary = {};
+    this.length = 0;
+  }
+  // This method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+  // This method will return all the values in the set
+  values() {
+    return Object.keys(this.dictionary);
+  }
+  // This method will add an element to the set
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = true;
+      this.length++;
+      return true;
+    }
+
+    return false;
+  }
+  // This method will remove an element from a set
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+  // This method will return the size of the set
+  size() {
+    return this.length;
+  }
+  // This is our union method 
+  union(set) {
+    const newSet = new Set();
+    this.values().forEach(value => {
+      newSet.add(value);
+    })
+    set.values().forEach(value => {
+      newSet.add(value);
+    })
+
+    return newSet;
+  }
+  // Only change code below this line
+  
+  // Only change code above this line
+}
+
+// Create a Priority Queue Class
+function PriorityQueue () {
+  this.collection = [];
+  this.printCollection = function() {
+    console.log(this.collection);
+  };
+  // Only change code below this line
+
+  // Only change code above this line
+}
+
+// Create and Add to Sets in ES6
+function checkSet() {
+  var set = new Set([1, 2, 3, 3, 2, 1, 2, 3, 1]);
+  // Only change code below this line
+
+  // Only change code above this line
+  console.log(Array.from(set));
+  return set;
+}
+
+checkSet();
+
+// Create a Map Data Structure
+var Map = function() {
+  this.collection = {};
+  // Only change code below this line
+  
+  // Only change code above this line
+};
