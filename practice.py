e2fa775e64752eb6670105231b8193cc39c46ccc
nhipqnguyen <quynhi2106@gmail.com>
2021-11-24 19:54:18 -0800

    ussc practice problem 1 solved

--- practice.py
+++ practice.py
@@ -1,1 +1,17 @@
 # 1-1. A psychologist wants to estimate the average dollar amount that a Visalia,
+# CA resident would donate to a local homeless shelter. Twenty residents were
+#randomly selected from the Visalia telephone directory which contains about
+#30,800 names. The 20 randomly residents were contacted by telephone and asked
+#how much they would be willing to donate. Their responses (in dollars) are given below.
+response = "30 20 15 10 10 60 20 25 20 30 10 5 50 40 20 10 10 0 20 50"
+
+# a) Describe the study population.
+# b) Describe the population mean (𝜇) in the context of this study.
+# c) A volunteer group has agreed to call all 30,800 residents and ask for a donation if there
+# is strong evidence to suggest that 𝜇 > 15. Test H0: 𝜇 = 15. Report t, df, p-value, and your decision based on 𝛼 = .05.
+# d) Compute a 95% confidence interval for 𝜇 and interpret the results.
+# e) A similar study is being planned for the city of San Luis Obispo. How many residents
+#should be contacted to obtain a 95% confidence interval for 𝜇 that has a width of $10.00?
+#Use the sample variance (the squared standard deviation) from the Visalia study as the
+#planning value for the population variance. Use R or hand compute Equation 1.6.
+
