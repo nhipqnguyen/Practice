6924ab3497807b375f98bab0cb094c7da5da32e0
nhipqnguyen <quynhi2106@gmail.com>
2021-11-29 23:53:00 -0800

    VBA technical questions

---
+++ technical_questions.js
@@ -0,0 +1,15 @@
+/* 
+Excel VBA
+What is the difference among COUNT, COUNTA, COUNTIF and COUNTBLANK in Excel?
+    COUNT is used to count cells containing numbers, dates, etc.
+    COUNTA is used to count any cell value containing numbers, text, logical values, etc. any type of value excluding blanks.
+    COUNTBLANK will count blank cells or cells containing an empty string.
+    COUNTIF and COUNTIFS count cells matching a certain criteria in the IF statement.
+
+Is it possible to make a pivot table using multiple sources of data?
+    Yes, but the multiple sources must at least be within the same workbook
+
+What filter should we use if you want more than two conditions?
+    The "Advanced Criteria Filter" will work for us to analyze the list or test more than two conditions.
+*/
+
