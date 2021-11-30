8f43e641f71dd398279194286172cc85e04dfc07
nhipqnguyen <quynhi2106@gmail.com>
2021-11-29 23:59:57 -0800

    python challenge

--- technical_questions.py
+++ technical_questions.py
@@ -9,3 +9,34 @@
 
 ## 3. What is "git stash"?
 ### Git stash takes a working directory and saves a copy of the changes so that you can reapply at any time if you need to.
+
+
+# python challenge
+##  Write a function that takes a string input: 'str'
+##  Return the number / count of vowels in the input string.
+##  For the purpose of this assignment, consider 'a', 'e', 'i', 'o' and 'u'
+##  as vowels
+##  the input string will consist of lower case letters
+
+## Pseudocode Solution
+# def count_the_vowels(str):
+    # initialize a counter variable  to 0
+    # loop through each character in the string
+    #  if the char matches one of the  vowels in upper case of lowercase
+
+    # increment counter by one
+    # return  the  counter
+
+## python solution
+def count_the_vowels(str):
+    # initialize a counter variable  to 0
+    num_vowels = 0
+    # loop through each character in the string
+    for char in str:
+        #  if the char matches one of the  vowels in upper case of     lowercase
+        if char in "aeiouAEIOU":
+            # increment counter by one
+            num_vowels = num_vowels + 1
+
+    # return  the  counter
+    return num_vowels
