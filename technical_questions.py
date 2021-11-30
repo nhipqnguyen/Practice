d3a45283212d7b93ef7068474750e73b0f3fcba5
nhipqnguyen <quynhi2106@gmail.com>
2021-11-29 23:57:37 -0800

    python challenge

---
+++ technical_questions.py
@@ -0,0 +1,11 @@
+# git
+## 1. What is a commit message and why are they important?
+### Commit messages are sent with each commit you make to a repository. 
+### They're important because they allow other team members to see what code was changed and what was accomplished in your work.
+
+## 2. What is a "git conflict"?
+### A git conflict arises when there are two separate branches that have made edits to the same line in a file, 
+### or if a file has been deleted in a branch but edited in another. Conflicts must be manually resolved before the branch can be merged into main.
+
+## 3. What is "git stash"?
+### Git stash takes a working directory and saves a copy of the changes so that you can reapply at any time if you need to.
