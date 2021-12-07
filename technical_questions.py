e0667b60372c39ff1a6260316652dc2c3edb00a7
nhipqnguyen <quynhi2106@gmail.com>
2021-12-06 21:31:21 -0800

    reformat python file

--- technical_questions.py
+++ technical_questions.py
@@ -74,50 +74,50 @@ def count_the_vowels(str):
 ### JSON and XML
 
 # SQL
-## 1. What is SQL and how is it different from NoSQL?
-### SQL databases are held in tables, and are relational; 
-### i.e. tables can be related to other tables in the database. 
-### NoSQL databases are usually document based, where key-value pairs are used to store data.
+    # 1. What is SQL and how is it different from NoSQL?
+        # SQL databases are held in tables, and are relational; 
+        # i.e. tables can be related to other tables in the database. 
+        # NoSQL databases are usually document based, where key-value pairs are used to store data.
 
-## 2. What is a primary key?
-### A primary key is either a single or combination of fields 
-### which uniquely identify a row in the table.
+    # 2. What is a primary key?
+        # A primary key is either a single or combination of fields 
+        # which uniquely identify a row in the table.
 
-## 3. What is a foreign key?
-### A foreign key in one table represents the primary key of another table. 
-### The relationship between the two is created by referencing each key.
+    # 3. What is a foreign key?
+        # A foreign key in one table represents the primary key of another table. 
+        # The relationship between the two is created by referencing each key.
 
-## 4. What joins can you tell me about?
-### Inner join: Inner joins return rows where there is at least one match of rows
-### Right join: Returns all the rows from the right hand table, even if there are no matches in the left
-### Left join: Reverse of right join
-### Full join
-#### Results in the combination of results from both left and right tables.
-#### It will contain all records from both tables.
+    # 4. What joins can you tell me about?
+        # Inner join: Inner joins return rows where there is at least one match of rows
+        # Right join: Returns all the rows from the right hand table, even if there are no matches in the left
+        # Left join: Reverse of right join
+        # Full join
+        # Results in the combination of results from both left and right tables.
+        # It will contain all records from both tables.
 
-## 5. What is a relationship and what kinds are there? 
-### Relationships are the associations between two columns in two or more tables. 
-### Table A will be associated with Table B in different ways. They are:
+    # 5. What is a relationship and what kinds are there? 
+        # Relationships are the associations between two columns in two or more tables. 
+        # Table A will be associated with Table B in different ways. They are:
 
-    ### 1. One-to-one
-        #### Defined as a direct relationship between two tables, 
-        #### a primary key on Table A will be associated with a foreign key on Table B; 
-        #### i.e. one author has only one place of birth.
+        # 1. One-to-one
+            # Defined as a direct relationship between two tables, 
+            # a primary key on Table A will be associated with a foreign key on Table B; 
+            # i.e. one author has only one place of birth.
     
-    ### 2. One-to-many
-        #### Defined as a relationship between two tables where 
-        #### a row from Table A matches multiple rows in Table B; 
-        #### i.e. one author can have many books.
+        # 2. One-to-many
+            # Defined as a relationship between two tables where 
+            # a row from Table A matches multiple rows in Table B; 
+            # i.e. one author can have many books.
 
-    # 3. Many-to-one
-        # The reverse of one-to-many, 
-        # where a row from Table B can match multiple from Table A.
+        # 3. Many-to-one
+            # The reverse of one-to-many, 
+            # where a row from Table B can match multiple from Table A.
 
-    # 4. Many-to-many
-        # Defined as a relationship between two tables where 
-        # multiple rows from Table A match multiple rows from Table B, 
-        # usually through Table C; i.e. where 
-        # Table A is a list of application users, 
-        # and table B is a list of books, 
-        # Table C would combine IDs from both tables to indicate 
-        # who owns which books in their personal library.
+        # 4. Many-to-many
+            # Defined as a relationship between two tables where 
+            # multiple rows from Table A match multiple rows from Table B, 
+            # usually through Table C; i.e. where 
+            # Table A is a list of application users, 
+            # and table B is a list of books, 
+            # Table C would combine IDs from both tables to indicate 
+            # who owns which books in their personal library.
